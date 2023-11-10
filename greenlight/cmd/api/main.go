be1a6eaf477d45a22bb5f336559d4135eba5e1e0
Pistifeju <juhasz.pisti7253@gmail.com>
2023-11-10 23:16:10 +0100

    Sending JSON Responses

--- greenlight/cmd/api/main.go
+++ greenlight/cmd/api/main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+const version = "1.0.0"
+
+type config struct {
+	port int
+	env  string
+}
+
+type application struct {
+	config config
+	logger *log.Logger
+}
 
 func main() {
-	fmt.Println("Hello world")
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 4000, "API Server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|stagin|production)")
+	flag.Parse()
+
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", app.config.port),
+		Handler:      app.routes(),
+		ReadTimeout:  time.Minute,
+		WriteTimeout: 10 * time.Minute,
+		IdleTimeout:  30 * time.Minute,
+	}
+
+	app.logger.Printf("starting %s server on %s", app.config.env, srv.Addr)
+	err := srv.ListenAndServe()
+	logger.Fatal(err)
 }
